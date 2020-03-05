73a837fc2fc85fa802bd9d0cf63783f377805d81
randy3019 <59527893+randy3019@users.noreply.github.com>
2020-03-05 20:07:45 +0700

    Add files via upload

---
+++ algorithm-sum-of-squares.py
@@ -0,0 +1,15 @@
+##input
+a = int(input("enter random number"))
+b = int(input("another random number"))
+##output
+c = a**2 + b**2
+
+print(c)
+
+##note:pseudecode
+
+## input: a,b, e z
+## ouput: c, dimana c = a**2 + b**2
+### c: a**2 + b**2;
+###  returnc;
+### end;
